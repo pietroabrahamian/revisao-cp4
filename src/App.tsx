528e5dd9026e43363412460d3b9dbdcb82fdbfd4
pietroabrahamian <pietroabrahamian2018@gmail.com>
2025-08-21 20:16:28 -0300

    feat: created footer-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Content } from './components/content'
+import { Footer } from './components/footer'
 import { Header } from "./components/header"
 
 function App() {
@@ -6,12 +7,15 @@ function App() {
 return (
     <>
       <Header />
+
       <Content 
       title='Aula de Front'
       content='aula de revisão do checkpoint'/>
       <Content 
       title='Aula de Pyhon'
       content='aula de dicionarios no python'/>
+
+      <Footer />
     </>
   )
 }
