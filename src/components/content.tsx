a6c3f51bb8270d23186bcca84f6bdeac0ae53b18
pietroabrahamian <pietroabrahamian2018@gmail.com>
2025-08-21 19:40:22 -0300

    feat: created content component

---
+++ src/components/content.tsx
@@ -0,0 +1,12 @@
+export function Content() {
+    return (
+        <section>
+            <div style={{
+                border: '1px solid #ccc'
+            }}>
+                <h3>Aula de Front</h3>
+                <p>aula de revisáo do checkpoitn 4</p>
+            </div>
+        </section>
+    )
+}
